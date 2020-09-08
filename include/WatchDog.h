be4d68336ca2a637b2d9d0ca51b32ea760ae75d6
Shankar <sangar@gmail.com>
2020-09-08 19:43:11 +0530

    Increasing the Heartbeat interval to 300 secs from 60 secs. Version bumped to 6

--- include/WatchDog.h
+++ include/WatchDog.h
@@ -25,10 +25,10 @@
 #define WHERE_R_U       (5)
 #define SMART_TV_MAC    (6)
 
-#define MAX_INTERVAL_SECs   (60)
+#define MAX_INTERVAL_SECs   (5 * 60) // 60 to 5*60 as per Dinesh on 2020-09-08
 #define GET_UP_TIME_SECs    (4 * MAX_INTERVAL_SECs)
 #define CHECK_INTERVAL_SECs (15)
-#define WATCHDOG_VERSION    (5)
+#define WATCHDOG_VERSION    (6)
 
 #define WPA_SUPPLICANT_FILE     "/etc/wpa_supplicant/wpa_supplicant.conf"
 //#define WPA_SUPPLICANT_FILE     "/home/tstone10/sgn/bkup/private/projs/SGNBundle/common/wpa_supplicant.conf"
